c4a134dc3c9d883adc7d6130e7efdc17af66a465
Georgi Petrov <petrov.d.george@gmail.com>
2023-03-18 12:08:31 +0200

    Initial commit

--- README.md
+++ README.md
@@ -21,3 +21,41 @@ This is a [Flask](https://flask.palletsprojects.com/en/1.1.x/) app that serves a
 
 - Install Python requirements `pip install -r requirements.txt`
 - Start the server for development `python3 main.py`
+
+## Endpoints
+
+### /data
+
+Returns historical stock or forex data.
+
+#### Example curl queries
+
+##### Stock
+
+    curl 'http://127.0.0.1:5000/data?symbol=AAPL&interval=1d&start=2023-02-01&end=2023-03-01'
+
+##### Forex (FRED)
+
+    curl 'http://127.0.0.1:5000/data?symbol=DEXUSEU&interval=1d&start=2023-02-01&end=2023-03-01'
+
+### /backtest
+
+Backtests a simple moving average crossover trading strategy on the given stock or forex data.
+
+#### Example curl queries
+
+##### Stock
+
+    curl 'http://127.0.0.1:5000/backtest?strategy=sma_crossover&symbol=MSFT&interval=1d&start=2021-01-01&end=2021-12-31&sma_short_period=10&sma_long_period=30'
+    
+
+##### Forex (FRED)
+
+    curl 'http://127.0.0.1:5000/backtest?strategy=sma_crossover&symbol=DEXUSEU&interval=1d&start=2023-02-01&end=2023-03-01&sma_short_period=50&sma_long_period=200'
+
+
+## Notes
+
+1. FRED data is limited and only provides daily frequency for specific currency pairs. If you need more comprehensive forex data, you may need to use a data provider that requires an API key, such as Oanda or Alpha Vantage.
+
+
--- main.py
+++ main.py
@@ -1,13 +1,150 @@
-from flask import Flask, jsonify
+from flask import Flask, request, jsonify
+import pandas as pd
+import numpy as np
+import yfinance as yf
+import ccxt
 import os
+import pandas_datareader.data as web
+
+def get_forex_data_fred(symbol, start, end):
+    data = web.get_data_fred(symbol, start, end)
+    
+    # Rename the 'VALUE' column to 'Close' for consistency
+    if 'VALUE' in data.columns:
+        data = data.rename(columns={'VALUE': 'Close'})
+
+    return data
+
+
+def get_forex_data(symbol, interval, start, end):
+    exchange = ccxt.oandav20({
+        'api_key': 'YOUR_API_KEY',
+    })
+
+    timeframe = map_interval_to_timeframe(interval)
+    since = exchange.parse8601(f"{start}T00:00:00Z")
+    to = exchange.parse8601(f"{end}T23:59:59Z")
+
+    ohlcv = exchange.fetch_ohlcv(symbol, timeframe, since, to)
+    data = pd.DataFrame(ohlcv, columns=['Timestamp', 'Open', 'High', 'Low', 'Close', 'Volume'])
+    data['Timestamp'] = pd.to_datetime(data['Timestamp'], unit='ms')
+
+    return data.set_index('Timestamp')
+
+# import matplotlib.pyplot as plt
 
 app = Flask(__name__)
 
+# Add the existing functions (get_stock_data, calculate_sma, sma_crossover_strategy, calculate_profitability) here
+def get_stock_data(ticker, interval, start, end):
+    stock_data = yf.download(ticker, start=start, end=end, interval=interval)
+    return stock_data
 
-@app.route('/')
-def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+def calculate_sma(data, period):
+    return data['Close'].rolling(window=period).mean()
 
+def sma_crossover_strategy(data, sma_short, sma_long):
+    signal = 0
+    position = None
+    buy_signals = []
+    sell_signals = []
+    
+    for i in range(len(data)):
+        if sma_short[i] > sma_long[i] and signal != 1:
+            signal = 1
+            position = data['Close'][i]
+            buy_signals.append(data.index[i])
+        elif sma_short[i] < sma_long[i] and signal != -1:
+            signal = -1
+            position = None
+            sell_signals.append(data.index[i])
+
+    return buy_signals, sell_signals
+
+def sma_crossover_strategy(data, sma_short, sma_long):
+    signal = 0
+    position = None
+    trades = []
+
+    for i in range(len(data)):
+        if sma_short[i] > sma_long[i] and signal != 1:
+            signal = 1
+            position = data.index[i]
+        elif sma_short[i] < sma_long[i] and signal != -1 and position is not None:
+            signal = -1
+            sell_timestamp = data.index[i]
+            trades.append((position, sell_timestamp))
+            position = None
+
+    return trades
+
+def calculate_profitability(trades):
+    returns = [(sell_price / buy_price - 1) for buy_price, sell_price in trades]
+    overall_return = np.prod([1 + r for r in returns]) - 1
+    return overall_return * 100
+
+@app.route('/data', methods=['GET'])
+def get_data():
+    symbol = request.args.get('symbol')
+    interval = request.args.get('interval')
+    start = request.args.get('start')
+    end = request.args.get('end')
+
+    is_forex = '/' in symbol
+    is_fred_forex = symbol.startswith('DEX')
+
+    if is_forex and not is_fred_forex:
+        data = get_forex_data(symbol, interval, start, end)
+    elif is_fred_forex:
+        data = get_forex_data_fred(symbol, start, end)
+    else:
+        data = get_stock_data(symbol, interval, start, end)
+
+    data.reset_index(inplace=True)
+    data_dict = data.to_dict(orient='records')
+
+    return jsonify(data_dict)
+
+@app.route('/backtest', methods=['GET'])
+def backtest():
+    strategy = request.args.get('strategy')
+    symbol = request.args.get('symbol')
+    interval = request.args.get('interval')
+    start = request.args.get('start')
+    end = request.args.get('end')
+    sma_short_period = int(request.args.get('sma_short_period'))
+    sma_long_period = int(request.args.get('sma_long_period'))
+
+    is_forex = '/' in symbol
+    is_fred_forex = symbol.startswith('DEX')
+
+    if is_forex and not is_fred_forex:
+        data = get_forex_data(symbol, interval, start, end)
+    elif is_fred_forex:
+        data = get_forex_data_fred(symbol, start, end)
+    else:
+        data = get_stock_data(symbol, interval, start, end)
+
+    if strategy == 'sma_crossover':
+        data['sma_short'] = calculate_sma(data, sma_short_period)
+        data['sma_long'] = calculate_sma(data, sma_long_period)
+        trades = sma_crossover_strategy(data, data['sma_short'], data['sma_long'])
+        profitability = calculate_profitability([(data['Close'][buy], data['Close'][sell]) for buy, sell in trades])
+
+        response = {
+            'profitability': round(profitability, 2),
+            'trades': [
+                {
+                    'buy_timestamp': str(buy_timestamp),
+                    'sell_timestamp': str(sell_timestamp),
+                }
+                for buy_timestamp, sell_timestamp in trades
+            ],
+        }
+
+        return jsonify(response)
+    else:
+        return jsonify({'error': 'Invalid strategy'}), 400
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
